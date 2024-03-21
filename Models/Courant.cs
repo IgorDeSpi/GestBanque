37d4f6218c43502927b988851c1e5aad623d5871
Igor <igor.de.spirlet@hotmail.com>
2024-03-21 15:41:06 +0100

    premier ajout de la partie compte courant

---
+++ Models/Courant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+
+public class Courant
+{
+    public string Numero;
+    private double _solde;
+    public double Solde 
+    { 
+        get
+        {
+            return _solde;
+        }
+        private set
+        {
+            if (value < 0)
+            {
+                return;
+            }
+            _solde = value;
+        } 
+    }
+
+    public double LigneDeCredit;
+    public Personne Titulaire;
+    public Courant(string numero, double solde, double ligneDeCredit, Personne titulaire)
+    {
+        Numero = numero;
+        Solde = solde;
+        LigneDeCredit = ligneDeCredit;
+        Titulaire = titulaire;
+    }
+
+    public void Retrait(double Montant)
+    {
+
+    }
+
+    public void Depot(double Montant)
+    {
+
+    }
+}
