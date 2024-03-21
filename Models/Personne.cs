3e1eaabecd2c10d36c20be242dc0855d0784f98d
Igor <igor.de.spirlet@hotmail.com>
2024-03-21 14:22:09 +0100

    Ajout class Personne

---
+++ Models/Personne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Personne
+    {
+        public string Nom;
+        public string Prenom;
+        public DateTime DateNaiss;
+        public Personne(string nom, string prenom, DateTime dateNaiss)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            DateNaiss = dateNaiss;
+        }
+    }
+}
